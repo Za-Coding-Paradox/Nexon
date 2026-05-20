2512fb6b0340c203c4c00b506720707a6d1ed04c
Za-Coding-Paradox <zacodingparadox@example.com>
2026-05-20 10:11:58 +0500

    FEAT: Testing Basics of Rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+trait Shape {
+    fn area(&self) -> f64;
+    fn describe(&self) {
+        println!("Area: {}", self.area());
+    }
+}
+struct Circle {
+    radius: f64
+}
+
+impl Shape for Circle {
+    fn area(&self) -> f64 {
+        std::f64::consts::PI * self.radius * self.radius
+    }
+}
+
+const constCircle: Circle = Circle { radius: 3 as f64 };
+fn main () {
+    constCircle.describe();
 }
