7942038060da010e42207cb9bc2842b32616a623
Za-Coding-Paradox <zacodingparadox@example.com>
2026-05-20 13:18:01 +0500

    FEAT: Updated Main after removing blockchain file

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,6 @@
 
 mod block;
 mod utils;
-
-use block::blockchain::BlockChain;
-
 fn main() {
-    let chain = BlockChain::new();
     println!("Nexon blockchain initialised.");
-    println!("Genesis block hash index: {}", chain.getLastBlock().header.index);
 }
